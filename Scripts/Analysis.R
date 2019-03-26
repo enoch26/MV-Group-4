6e6321e5817f068313782b276c79dffee8a65041
Brooke Franklin <fab.mouse@hotmail.com>
2019-03-26 16:38:13 +0000

    New script for an multivariate analysis - uses the clean dataset

---
+++ Scripts/Analysis.R
@@ -0,0 +1,10 @@
+#################################################################
+#ANAYSIS SCRIPT: USE THIS SCRIPT TO CARRYOUT MULTIVARIATE METHODS
+#################################################################
+
+# IMPORT DATASET
+imdbData_Clean <- read.csv("Data/Clean IMDb Data.csv")
+
+# LOAD LIBARARIES
+
+# PRINCIPLE COMPONENT ANALYSIS -------------------------------------------------
