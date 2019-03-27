8776333e3a5e0c7f5c62a565ec698bf1f4518afa
enoch26 <enochsuen26@gmail.com>
2019-03-27 15:49:17 +0000

    PCA analysis

--- Scripts/Analysis.R
+++ Scripts/Analysis.R
@@ -8,3 +8,10 @@ imdbData_Clean <- read.csv("Data/Clean IMDb Data.csv")
 # LOAD LIBARARIES
 
 # PRINCIPLE COMPONENT ANALYSIS -------------------------------------------------
+# extract numeric attributes
+num <- imdbData_Clean[sapply(imdbData_Clean, function(x) is.numeric(x))]
+str(num)
+# PCA
+pmovie <- princomp(na.omit(num), scores=T, cor=T)
+summary(pmovie)
+screeplot(pmovie,type="l")
