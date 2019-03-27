908185dbac8c004a2d704969bca1c7bea2a30e7c
enoch26 <enochsuen26@gmail.com>
2019-03-27 16:00:15 +0000

    PCA update

--- Scripts/Analysis.R
+++ Scripts/Analysis.R
@@ -33,16 +33,3 @@ plot(pmovie$scores[,1],pmovie$scores[,2],
 text(pmovie$scores[,1],pmovie$scores[,2],
      labels=abbreviate(imdbData_Clean[,20]),  # to select the factor var
      cex=0.1,lwd=2)
-names(imdbData_Clean[,3])
-||||||| merged common ancestors
-# PRINCIPLE COMPONENT ANALYSIS -------------------------------------------------
-=======
-# PRINCIPLE COMPONENT ANALYSIS -------------------------------------------------
-# extract numeric attributes
-num <- imdbData_Clean[sapply(imdbData_Clean, function(x) is.numeric(x))]
-str(num)
-# PCA
-pmovie <- princomp(na.omit(num), scores=T, cor=T)
-summary(pmovie)
-screeplot(pmovie,type="l")
->>>>>>> 8776333e3a5e0c7f5c62a565ec698bf1f4518afa
