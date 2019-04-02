28d86800579b416e0e496eed4a0f0f62ddad1d7c
enoch26 <enochsuen26@gmail.com>
2019-04-02 21:23:08 +0100

    Network graph

--- Scripts/Analysis.R
+++ Scripts/Analysis.R
@@ -6,6 +6,8 @@
 imdbData_Clean <- read.csv("Data/Clean IMDb Data.csv")
 glimpse(imdbData_Clean)
 # LOAD LIBARARIES --------------------------------------------------------------
+library(corrplot)
+library(qgraph)
 
 # PRINCIPLE COMPONENT ANALYSIS -------------------------------------------------
 # Extract numeric attributes
@@ -30,8 +32,7 @@ pscores <- pmovie$scores
 pload <- pmovie$loadings
 
 pload[, 1:8]
-# cast_total_facebook_likes in PC2 noteworthy
-
+# cast_total_facebook_likes dominates in PC2
 plot(pmovie$scores[,2],na.omit(numData)$cast_total_facebook_likes)
 
 # Exploring Important Vairbales
@@ -43,16 +44,30 @@ length(which(abs(pload[, 1]) > eCriterion)) #Suggests 7 variables
 mCriterion <- 0.7 * max(pload[, 1])
 length(which(abs(pload[, 1]) > mCriterion)) #Suggests 6 variables
 
-
-# Biplot
-biplot(pmovie,  )
-
 # Visual Exploration of principal components 
 par(pty = "s")
-plot(pmovie$scores[, 1], pmovie$scores[, 2],
-     ylim = range(pmovie$scores[, 1]),
-     xlab = "PC1", ylab = "PC2", type = "n", lwd = 2)
+# plot 1:19 var after 19 error
+for (i in (1:19)) {
+  plot(pmovie$scores[, 1], pmovie$scores[, 2],
+  ylim = range(pmovie$scores[, 1]),
+  xlab = "PC1", ylab = "PC2", col = imdbData_Clean[, i], cex=0.5, lwd = 2, 
+  main = paste(names(imdbData_Clean)[i]))
+}
+
 text(pmovie$scores[, 1], pmovie$scores[, 2],
-     labels = abbreviate(imdbData_Clean[, 1]),  # to select the factor var
+     labels = imdbData_Clean[, 1],  # to select the factor var
      cex = 0.1, lwd = 2)
 
+text(pmovie$scores[, 1], pmovie$scores[, 2],
+     labels = abbreviate(imdbData_Clean[, ]),  # to select the factor var
+     cex = 0.1, lwd = 2)
+
+# Biplot
+biplot(pmovie, xlabs=na.omit(imdbData_Clean)$content_rating, 
+       choices = c(1,2))
+# show two main directions except title year 
+
+# Network graph
+corrplot(cor(na.omit(numData)), method="ellipse")
+qgraph(cor(na.omit(numData)))
+# all positive correlated
